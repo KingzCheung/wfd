f1376b88fa569d0e51818e6a8480ffd5ef7d7278
kingzcheung@gmail.com <kINg732351>
2017-02-19 22:14:40 +0800

    add files

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+from flask import flash
+from flask import redirect
+
 from app import app
 from flask import render_template
+
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -10,3 +16,14 @@ def index():
     return render_template("index.html",
                            title='Home',
                            user=user)
+
+@app.route('/login',methods=['GET','POST'])
+
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+
+    return render_template('login.html',title = 'Sign in',form = form)
