bfdc423443934c3b3adfa03a386576c8652b2a71
KingzCheung <kingzcheung@gmail.com>
2017-03-14 11:52:45 +0800

    添加登录

---
+++ src/component/common/Cell.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="cell">
+        <div class="w-cells">
+            <a href="javascript:;">
+                aaa
+            </a>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+    @import "../../scss/base.scss";
+</style>
+
+<script>
+
+
+    export default {
+        name: 'w-cell',
+        data () {
+            return {}
+        },
+
+    }
+</script>
