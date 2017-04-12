bbf79f0d668601e5586c08e585e67d6f9e330f17
KingzCheung <kingzcheung@gmail.com>
2017-04-12 22:51:01 +0800

    1.添加分类数据 2.修改分类效果

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -2,17 +2,14 @@
     <div id="category">
         <w-head message="分类"></w-head>
         <div class="container">
+            <dl class="listgroup" v-for="(item,i,index) in categorys['root']" :key="index">
+                <dt>{{item.cname}}</dt>
 
-            <grids>
-                <grid-list :to="item.to" :text="item.text" v-for="item in categorys" :key="item.text">
-                    <div slot="icon">
-                        <svg class="icon" aria-hidden="true">
-                            <use xlink:href="#icon-set"></use>
-                        </svg>
-                    </div>
-                </grid-list>
-            </grids>
+                <dd v-for="(subitem,subi,subindex) in categorys['sub']" :key="subindex" v-if="subitem.pid == item.id">
+                    <a href="#">{{subitem.cname}}</a>
+                </dd>
 
+            </dl>
         </div>
         <w-tab></w-tab>
     </div>
@@ -23,28 +20,20 @@
 </style>
 
 <script>
-    import grids from '../component/common/Grids.vue';
-    import grid from '../component/common/Grid.vue';
     export default {
         name: 'category',
         data () {
             return {
-                categorys: [
-                    {to: '/', text: "第1个"},
-                    {to: '/category', text: "第2个"},
-                    {to: '/category', text: "第3个"},
-                    {to: '/category', text: "第4个"},
-                    {to: '/category', text: "第5个"},
-                    {to: '/category', text: "第6个"},
-                    {to: '/category', text: "第7个"},
-                    {to: '/category', text: "第8个"},
-                    {to: '/category', text: "第9个"}
-                ]
+                categorys: {
+                    root: null, sub: null
+                }
             }
         },
-        components: {
-            'grids': grids,
-            'grid-list': grid
+        created: function () {
+            let self = this;
+            self.$http.get(self.$config.api('category')).then(function (resp) {
+                self.categorys = resp.data;
+            });
         }
 
     }
